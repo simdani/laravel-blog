302dbd88fe73879783499fddcb991028a7d37d5a
Simonas <dranxman@gmail.com>
2017-10-07 13:16:45 +0300

    add tasks controller

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,4 @@
 <?php
 
-use App\Task;
-
-Route::get('/tasks', function () {
-
-    $tasks = Task::all();
-
-    return view('tasks.index', compact('tasks'));
-}); 
-
-Route::get('/tasks/{id}', function($id) {
-    //$task = DB::table('tasks')->find($id);
-    $task = Task::find($id);
-    
-    return view('tasks.show', compact('task'));
-});
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{task}', 'TasksController@show');
